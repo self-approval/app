40cefd22b6187f87215b1fcc94e12e742ee75e8c
Cubik65536 <cubik65536@cubik65536.top>
2022-08-14 22:21:12 -0400

    test: Add test for status when Comment is not created by Pull Request author

--- test/index.test.ts
+++ test/index.test.ts
@@ -68,6 +68,41 @@ describe("self-approve bot", () => {
     expect(nock.isDone()).toBeTruthy();
   });
 
+  test("Comment is not created by Pull Request author", async () => {
+    const payload = require("./fixtures/pull_request.commented.not-same-user.json");
+    const config = "self_approval_comments:\n  - \"I self-approve!\"\nfrom_author:\n  - Cubik65536\napply_labels:\n  - \"can-be-merged\"";
+
+    nock("https://api.github.com")
+      .get(
+        "/repos/CubikTech/self-approval/contents/.github%2Fself-approval.yml"
+      )
+      .reply(200, config);
+
+    // Send not allowed message
+    nock("https://api.github.com")
+        .post(
+          "/repos/CubikTech/self-approval/issues/1/comments", (body: any) => {
+            expect(body.body).toBe("You are not allowed to self-approve others Pull Request!");
+            return true
+          })
+        .reply(200);
+
+    // React with "-1" reaction
+    nock("https://api.github.com")
+        .post(
+          "/repos/CubikTech/self-approval/issues/comments/1214464178/reactions", (body: any) => {
+            expect(body.content).toBe("-1");
+            return true
+          })
+        .reply(200);
+
+    // Receive a webhook event
+    await probot.receive({ name: "issue_comment", payload });
+
+    await new Promise(process.nextTick); // Don't assert until all async processing finishes
+    expect(nock.isDone()).toBeTruthy();
+  });
+
   afterEach(() => {
     nock.cleanAll();
     nock.enableNetConnect();
