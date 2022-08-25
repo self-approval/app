1a39e2933e44cc8284f86fe6cdcf43b7f8ff0ad2
Cubik65536 <cubik65536@cubik65536.top>
2022-08-24 20:37:09 -0400

    test: Add test for status when Pull Request is approved

--- test/index.test.ts
+++ test/index.test.ts
@@ -138,6 +138,48 @@ describe("self-approve bot", () => {
     expect(nock.isDone()).toBeTruthy();
   });
 
+  test("Pull Request is approved", async () => {
+    const payload = require("./fixtures/pull_request.commented.approved.json");
+    const config = "self_approval_comments:\n  - \"I self-approve!\"\nfrom_author:\n  - Cubik65536\napply_labels:\n  - \"can-be-merged\"";
+
+    nock("https://api.github.com")
+      .get(
+        "/repos/CubikTech/self-approval/contents/.github%2Fself-approval.yml"
+      )
+      .reply(200, config);
+
+    // React with "+1" reaction
+    nock("https://api.github.com")
+        .post(
+          "/repos/CubikTech/self-approval/issues/comments/1214464178/reactions", (body: any) => {
+            expect(body.content).toBe("+1");
+            return true
+          })
+        .reply(200);
+
+    // Pull Request is approved
+    nock('https://api.github.com')
+        .post('/repos/CubikTech/self-approval/pulls/1/reviews', (body: any) => {
+          return body.event === 'APPROVE'
+        })
+        .reply(200)
+
+    // React with "hooray" reaction
+    nock("https://api.github.com")
+        .post(
+          "/repos/CubikTech/self-approval/issues/comments/1214464178/reactions", (body: any) => {
+            expect(body.content).toBe("hooray");
+            return true
+          })
+        .reply(200);
+
+    // Receive a webhook event
+    await probot.receive({ name: "issue_comment", payload });
+
+    await new Promise(process.nextTick); // Don't assert until all async processing finishes
+    expect(nock.isDone()).toBeTruthy();
+  });
+
   afterEach(() => {
     nock.cleanAll();
     nock.enableNetConnect();
