868e9506d3854901589f5f026517e150249a9ac3
Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>
2024-01-08 17:45:49 -0500

    refactor: Trigger the app on issue comment creation
    Signed-off-by: Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import { IsMessageForApp } from "./lib/is-message-for-app";
 export default (app: Probot) => {
   app.log("Yay! The app was loaded!");
 
-  app.on("issues.opened", async (context: Context) => {
+  app.on("issue_comment.created", async (context: Context) => {
     if (!new IsMessageForApp(context).verify()) {
       return context.octokit.issues.createComment(
         context.issue({ body: "This is not a message for me." })
