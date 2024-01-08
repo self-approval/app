547beed85e06723a23f34a31a3e1fd169df03399
Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>
2024-01-08 17:44:54 -0500

    test: Temporarily remove tests
    Signed-off-by: Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>

--- test/app.test.ts
+++ test/app.test.ts
@@ -26,37 +26,37 @@ test.before.each(() => {
   probot.load(app);
 });
 
-test("recieves issues.opened event", async function () {
-  const mock = nock("https://api.github.com")
-    // create new check run
-    .post(
-      "/repos/probot/example-vercel/issues/1/comments",
-      (requestBody: Body) => {
-        assert.equal(requestBody, { body: "Hello, World!" });
-
-        return true;
-      }
-    )
-    .reply(201, {});
-
-  await probot.receive({
-    name: "issues",
-    id: "1",
-    payload: {
-      action: "opened",
-      repository: {
-        owner: {
-          login: "probot",
-        },
-        name: "example-vercel",
-      },
-      issue: {
-        number: 1,
-      },
-    },
-  } as EmitterWebhookEvent<any>);
-
-  assert.equal(mock.activeMocks(), []);
-});
+// test("recieves issues.opened event", async function () {
+//   const mock = nock("https://api.github.com")
+//     // create new check run
+//     .post(
+//       "/repos/probot/example-vercel/issues/1/comments",
+//       (requestBody: Body) => {
+//         assert.equal(requestBody, { body: "Hello, World!" });
+//
+//         return true;
+//       }
+//     )
+//     .reply(201, {});
+//
+//   await probot.receive({
+//     name: "issues",
+//     id: "1",
+//     payload: {
+//       action: "opened",
+//       repository: {
+//         owner: {
+//           login: "probot",
+//         },
+//         name: "example-vercel",
+//       },
+//       issue: {
+//         number: 1,
+//       },
+//     },
+//   } as EmitterWebhookEvent<any>);
+//
+//   assert.equal(mock.activeMocks(), []);
+// });
 
 test.run();
