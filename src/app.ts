db43ac0dc9892007156f85d1e52cf1e1efc50fa0
Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>
2024-01-08 18:11:42 -0500

    feat: Complete context checker & Add user getter
    Signed-off-by: Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,15 @@
-import type { Probot, Context } from "probot";
+import type { Probot } from "probot";
 import { IsMessageForApp } from "./lib/is-message-for-app";
 
 export default (app: Probot) => {
   app.log("Yay! The app was loaded!");
 
-  app.on("issue_comment.created", async (context: Context) => {
-    if (context.isBot) return;
+  app.on("issue_comment.created", async (context) => {
+    if (!context.payload.issue.pull_request) return;  // ignore non-PR comments
+    if (context.isBot) return;  // ignore bot comments
+    if (!new IsMessageForApp(context).verify()) return; // ignore comments that don't mention the app
 
-    if (!new IsMessageForApp(context).verify()) {
-      return context.octokit.issues.createComment(
-        context.issue({ body: "This is not a message for me." })
-      );
-    }
-    return context.octokit.issues.createComment(
-      context.issue({ body: "This is a message for me." })
-    );
+    const issueUser = context.payload.issue.user.login;
+    const reviewUser = context.payload.comment.user.login;
   });
 };
