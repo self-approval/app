9072718e056eb4c4c2d5f3171d9b23f738772d6f
Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>
2024-01-10 18:48:50 -0500

    feat: Add other condition checks
    Signed-off-by: Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import type { Probot } from "probot";
 import { IsMessageForApp } from "./lib/is-message-for-app";
+import { PullRequestUtils } from "./lib/pull-request-utils";
 
 export default (app: Probot) => {
   app.log("Yay! The app was loaded!");
 
   app.on("issue_comment.created", async (context) => {
-    if (!context.payload.issue.pull_request) return;  // ignore non-PR comments
+    // if (!context.payload.issue.pull_request) return;  // ignore non-PR comments
     if (context.isBot) return;  // ignore bot comments
     if (!new IsMessageForApp(context).verify()) return; // ignore comments that don't mention the app
 
@@ -13,8 +14,8 @@ export default (app: Probot) => {
     const config: any = await context.config('self-approval.yml');
 
     // The pull request and comment creators
-    const issueUser = context.payload.issue.user.login;
-    const reviewUser = context.payload.comment.user.login;
+    const issueUser = context.payload.issue.user;
+    const reviewUser = context.payload.comment.user;
 
     // Get the comment sent by the user
     const comment = context.payload.comment.body;
@@ -33,6 +34,40 @@ export default (app: Probot) => {
         repo: context.payload.repository.name,
         comment_id: context.payload.comment.id,
         content: "confused"
+      });
+    }
+
+    // If the creator of the pull request not the same as the creator of the comment,
+    // add a -1 reaction and add a comment telling the user not to self-approve someone else's PR
+    if (issueUser.id !== reviewUser.id) {
+      context.octokit.reactions.createForIssueComment({
+        owner: context.payload.repository.owner.login,
+        repo: context.payload.repository.name,
+        comment_id: context.payload.comment.id,
+        content: "-1"
+      });
+      return context.octokit.issues.createComment({
+        owner: context.payload.repository.owner.login,
+        repo: context.payload.repository.name,
+        issue_number: context.payload.issue.number,
+        body: "@" + reviewUser.login + ", you are not allowed to self-approve someone else's PR."
+      });
+    }
+
+    // If the creator of the pull request and the comment is not in the list of allowed users,
+    // add a -1 reaction and add a comment telling the user they are not allowed to self-approve
+    if (!config.from_author.includes(issueUser.login)) {
+      context.octokit.reactions.createForIssueComment({
+        owner: context.payload.repository.owner.login,
+        repo: context.payload.repository.name,
+        comment_id: context.payload.comment.id,
+        content: "-1"
+      });
+      return context.octokit.issues.createComment({
+        owner: context.payload.repository.owner.login,
+        repo: context.payload.repository.name,
+        issue_number: context.payload.issue.number,
+        body: "@" + reviewUser.login + ", you are not allowed to self-approve your own PR."
       });
     }
   });
